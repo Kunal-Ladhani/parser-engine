38da1ba8d705e64f44d8b996d2a1398f0ccad45a
Kunal Ladhani <k.ladhani1@gmail.com>
2025-05-22 22:00:04 +0530

    removed all tests

--- src/test/java/com/parser/engine/ParserEngineApplicationTests.java
+++ src/test/java/com/parser/engine/ParserEngineApplicationTests.java
@@ -1,13 +1,8 @@
 package com.parser.engine;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ParserEngineApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
